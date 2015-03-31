829bcf6b4240e6c6183b604b3558de982ec74c03
Pan Otlet <otlet0@gmail.com>
2015-03-31 13:15:15 +0200

    Twitch player

--- config.php
+++ config.php
@@ -12,22 +12,42 @@ if(!defined("tag"))
 //Konfiguracja zaawansowana
 define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'dev');
 $system_path    = 'inc';
+$twitch_player  =   <<<EOF
+                            <object bgcolor="#000000"
+                                    data="//www-cdn.jtvnw.net/swflibs/TwitchPlayer.swf"
+                                    height="75%"
+                                    type="application/x-shockwave-flash"
+                                    width="100%"
+                                    >
+                              <param name="allowFullScreen"
+                                      value="true" />
+                              <param name="allowNetworking"
+                                      value="all" />
+                              <param name="allowScriptAccess"
+                                      value="always" />
+                              <param name="movie"
+                                      value="//www-cdn.jtvnw.net/swflibs/TwitchPlayer.swf" />
+                              <param name="flashvars"
+                                      value="channel=Kagaminium&auto_play=true&start_volume=25" />
+                            </object>
+EOF;
 
 //Konfiguracja podstawowa
 $style  =   'parallaxjs';
 $fb     =   "Kagaminium";
 $token  =   "CAANtbEzcjRUBAOk4G6vwi5ZCZCqTjsF5XZCrZAWKuvSf1enmINRhKKs6hNoXw22fyh1Jv7h7qkk4E1BUtYiHyO36Ou1Y7jxcK5ZAnsZCG8Wm7Be5a0vIKC2ZB3hHm7dVHGLIPScPs6CLjZAEYTEPwfljHewmR1eeDqpksFChL4tZAKavIWksdZCMvrE0dpDBHolicZBCZAGltyZCBOTZBxv2wf3Q4KewsKoE6y1CIZD";
 $config =   [
-    'owner'           =>  'Kagaminium',
-    'mail'            =>  'otlet@jest.guru',
-    'description'     =>  'Tutaj jakiś opis',
-    'aboutme'         =>  'Na imię mi Rafał inaczej znany jako "Kagami"',
-    'aboutme_content' =>  "Skoro już nolife'uje no czemu nie streamować. Mam w życiu 26 level, gram od czasów Snesa,
-                          poprzez takie konsole jak PSX, PS2, PS3, Xbox, xboxa360, Dreamcasta (nawet 2 razy) Obecnie nie posiadam konsoli,
-                          podejrzewam że zmieni się to gdy zostanie zapowiedziane Gran Turismo 7
-                          Oglądam sporo anime, a i dobry film/serial obejrzę.
-                          Jak stream odpali to pierwszymi rzeczami na jakie uzbieram to 2 monitor,
-                          konkretna kamerka oraz w przyszłości drugi komputer do enkodowania streama.",
-    'picture_path'    =>  'src="{tpl}demo/assets/Classy Skaught.jpg"',
-    'picture_text'    =>  'To jest podpis pod zdjęciem.',
+    'owner'             =>  'Kagaminium',
+    'mail'              =>  'otlet@jest.guru',
+    'description'       =>  'Tutaj jakiś opis',
+    'aboutme'           =>  'Na imię mi Rafał inaczej znany jako "Kagami"',
+    'aboutme_content'   =>  "Skoro już nolife'uje no czemu nie streamować. Mam w życiu 26 level, gram od czasów Snesa,
+                              poprzez takie konsole jak PSX, PS2, PS3, Xbox, xboxa360, Dreamcasta (nawet 2 razy) Obecnie nie posiadam konsoli,
+                              podejrzewam że zmieni się to gdy zostanie zapowiedziane Gran Turismo 7
+                              Oglądam sporo anime, a i dobry film/serial obejrzę.
+                              Jak stream odpali to pierwszymi rzeczami na jakie uzbieram to 2 monitor,
+                              konkretna kamerka oraz w przyszłości drugi komputer do enkodowania streama.",
+    'picture_path'      =>  'src="{tpl}demo/assets/Classy Skaught.jpg"',
+    'picture_text'      =>  'To jest podpis pod zdjęciem.',
+    'twitch_player'     =>  $twitch_player,
 ];
--- content/theme/parallaxjs/index.php
+++ content/theme/parallaxjs/index.php
@@ -48,21 +48,11 @@
 		</div>
 
 		<div id="sandbox" class="leaguegothic white page">
-			<a class="noanchor" href="https://github.com/stolksdorf/Parallaxjs"><div id="logo" class="f2">{owner}</div></a>
+			<a class="noanchor" href="#"><div id="logo" class="f2">Live Stream</div></a>
 
 			<div class="f0 center w750 pad">
 
-				<div class="f3">Examples</div>
-
-				<div id="fromLast" class="clickable f1">Returns you to the last page you were on</div>
-				<div id="fromCurrent" class="clickable f1">Alert from the current page</div>
-				<div id="fromRight" class="clickable f1">From the right slow</div>
-				<div id="fromTop" class="clickable f1">From the top with linear easing</div>
-				<div id="fromLeft" class="clickable f1">From the left with increased parallax</div>
-				<div id="fromBottom" class="clickable f1">From the bottom with an onload function</div>
-
-				<pre id = "codebox" class="black">Hover to see code.
-Click to see it in action!</pre>
+				{twitch_player}
 
 			</div>
 

ad2ee6caa73c953e56e34ccb541393a499fd6c1e
Siewo23 <valentinomagnat@gmail.com>
2015-04-13 18:48:37 +0200

    Refactor code & repair navigation :+1:
    - uporządkowanie kodu w pliku index.php;
    - poprawienie w nawigacji przyrównania tekstu ("left" dla lewej strzałki
    oraz right dla prawej strzałki);
    
    PS. Nie wiem kto i po jakiego grzyba, mieszał przy nawigacji w pliku
    index.php, skoro wszystkie daklaracje i sterowanie jest w index.js.

--- content/theme/parallaxjs/index.php
+++ content/theme/parallaxjs/index.php
@@ -1,32 +1,34 @@
 <html>
+
 <head>
+
+	<!-- Tytuł strony -->
 	<title>{owner}</title>
+
+	<!-- Ładowanie skryptów -->
 	<script type="text/javascript" src="{tpl}demo/libs/jq.js"></script>
 	<link rel="Stylesheet" type="text/css" href="{tpl}demo/libs/xo.css" />
-
 	<script type="text/javascript" src="{tpl}parallax.min.js"></script>
 	<script type="text/javascript" src="{tpl}/index.js"></script>
 	<script type="text/javascript" src="{tpl}demo/examples.js"></script>
 	<link rel="Stylesheet" type="text/css" href="{tpl}demo/index.css" />
-
 	<script type="text/javascript">
-
 	  var _gaq = _gaq || [];
 	  _gaq.push(['_setAccount', 'UA-34546066-1']);
 	  _gaq.push(['_trackPageview']);
-
 	  (function() {
 	    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
 	    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
 	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 	  })();
-
 	</script>
-</head>
-<body class="background">
 
+</head>
+
+<body class="background">
 	<div class="container">
 
+		<!-- Index - div startowy -->
 		<div id="index" class="leaguegothic white page">
 			<div id="titlebox" class="f2 lghtgrey">
 				<div class="f5 white">{owner}</div>
@@ -34,42 +36,44 @@
 			</div>
 		</div>
 
+		<!-- O mnie - div boczny (lewy) -->
 		<div id="me" class="leaguegothic white page f2">
-
 			<div class="w960 center" style="margin-top:50px;">
-
-			<div class="right"><img {picture_path} style="width:200px;"><br><span class="calibri f0">{picture_text}</span></div>
-
-			<h3>{aboutme}</h3>
-			<div id="siewo" class="calibri f1 center w850 pad">
-			{aboutme_content}
-		</div>
+				<div class="right"><img {picture_path} style="width:200px;"><br><span class="calibri f0">{picture_text}</span></div>
+				<h3>{aboutme}</h3>
+				<div id="siewo" class="calibri f1 center w850 pad">
+					{aboutme_content}
+				</div>
 			</div>
 		</div>
 
+		<!-- Facebook - div boczny (prawy) -->
+		<div id="features" class="leaguegothic white page">
+			<div id="content" class="black whitebg calibri f0 center w750 pad">
+				<h1>Kto to jest {owner}</h1>
+				<p>
+					{fbfeeds}
+				</p>
+			</div>
+		</div>
+
+		<!-- Twitch (Live Stream) - div górny -->
 		<div id="sandbox" class="leaguegothic white page">
-
 			<a class="noanchor" href="#"><div id="logo" class="f2">Live Stream</div></a>
-
 			<div class="f0 center w1280 pad">
-
 				{twitch_player}
-
 			</div>
-
 		</div>
 
+		<!-- Brak treści - div dolny -->
 		<div id="bottom" class="leaguegothic white page">
 			<div id="logo" class="f2">^hello^ {owner}</div></a>
-
 			<div class="f0 center w750 pad">
 
-
-
 			</div>
-
 		</div>
 
+		<!-- Fuknkcja Testowa -->
 		<div id="boo" class="leaguegothic white page clickable">
 			<div class="center w400 " style="margin-top:20%;">
 				<div class="lh0" style="font-size:300px;">BOO!</div>
@@ -77,43 +81,29 @@
 			</div>
 		</div>
 
-		<div id="features" class="leaguegothic white page">
-			<div id="content" class="black whitebg calibri f0 center w750 pad">
-
-				<h1>Kto to jest {owner}</h1>
-				<p>
-					{fbfeeds}
-				</p>
-
-			</div>
-
-
-		</div>
-
 	</div>
 
-
+	<!-- Nawigacja -->
 	<div id="leftControl" class="control">
 		<img id="leftArrow" class="arrow left" style="width:60px" src="{tpl}demo/assets/arrowleft2.png"></img>
-		<div id="leftText" class="dirText" style="float:right"> Test</div>
+		<div id="leftText" class="dirText" style="float:left"></div>
 	</div>
 
 	<div id="rightControl" class="control">
 		<img id="rightArrow" class="arrow right" style="width:60px" src="{tpl}demo/assets/arrowright2.png"></img>
-		<div id="rightText" class="dirText" style="float:left"> Test</div>
+		<div id="rightText" class="dirText" style="float:right"></div>
 	</div>
 
 	<div id="topControl" class="control center" style="width:145px;left:45%;">
 		<img id="topArrow" class="arrow left" style="height:60px" src="{tpl}demo/assets/arrowtop2.png"></img>
-		<div id="topText" class="dirText txtcenter" > Test</div>
+		<div id="topText" class="dirText txtcenter" ></div>
 	</div>
 
 	<div id="bottomControl" class="control center" style="width:145px;left:45%;bottom:0px;">
-		<div id="bottomText" class="dirText txtcenter"> Test</div>
+		<div id="bottomText" class="dirText txtcenter"></div>
 		<img id="bottomArrow" class="arrow left" style="height:60px" src="{tpl}demo/assets/arrowbottom2.png"></img>
-
 	</div>
 
-
 </body>
+
 </html>

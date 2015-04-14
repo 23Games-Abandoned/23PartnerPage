0db3598fd12984238aa8586d7c603fd272601f9e
Pan Otlet <otlet0@gmail.com>
2015-04-14 12:00:11 +0200

    Stil bad worg proparly give me dick

--- content/theme/parallaxjs/index.php
+++ content/theme/parallaxjs/index.php
@@ -63,8 +63,13 @@
 				<p>
 					{fbfeeds}
 				</p>
-                <div class="fb-comments" data-href="http://kagaminium.23games.me" data-width="100%" data-numposts="10" data-colorscheme="dark"></div>
-			</div>
+                <table class="table table-striped table-bordered bootstrap-datatable datatable dataTable" style="width:100%;" id="DataTables_Table_0" aria-describedby="DataTables_Table_0_info">
+                    <thead>
+                    <tr role="row"><th style="width:150px;" class="sorting_asc" role="columnheader" tabindex="0" aria-controls="DataTables_Table_0" rowspan="1" colspan="1" aria-sort="ascending" aria-label="Command: activate to sort column descending">Command</th><th class="sorting" role="columnheader" tabindex="0" aria-controls="DataTables_Table_0" rowspan="1" colspan="1" aria-label="Response: activate to sort column ascending">Response</th><th style="width:100px;" class="sorting" role="columnheader" tabindex="0" aria-controls="DataTables_Table_0" rowspan="1" colspan="1" aria-label="Userlevel: activate to sort column ascending">Userlevel</th></tr>
+                    </thead>
+
+                    <tbody id="commandsTableBody" role="alert" aria-live="polite" aria-relevant="all"><tr class="odd"><td class=" sorting_1">!animelist</td><td class="">http://myanimelist.net/animelist/kagami_sama</td><td class="">Everyone</td></tr><tr class="even"><td class=" sorting_1">!battletag</td><td class="">Kagamisama#1468</td><td class="">Everyone</td></tr><tr class="odd"><td class=" sorting_1">!fb</td><td class="">https://www.facebook.com/kagaminium?fref=ts</td><td class="">Everyone</td></tr><tr class="even"><td class=" sorting_1">!heynight</td><td class="">Cześć [touser]! HeyGuys</td><td class="">Regular</td></tr><tr class="odd"><td class=" sorting_1">!justice</td><td class="">De Batyl, De Batyl, De Batyl... Justice!</td><td class="">Everyone</td></tr><tr class="even"><td class=" sorting_1">!karikov</td><td class="">/me MISZA! Kreygasm</td><td class="">Everyone</td></tr><tr class="odd"><td class=" sorting_1">!kebab</td><td class="">proszę tbBaconBiscuit</td><td class="">Everyone</td></tr><tr class="even"><td class=" sorting_1">!koniecstreama</td><td class="">BibleThump BibleThump KONIEC BibleThump BibleThump</td><td class="">Everyone</td></tr><tr class="odd"><td class=" sorting_1">!lethal</td><td class="">Łan dimejdż of lifal - Kagami http://i.imgur.com/iIozj2c.gif</td><td class="">Everyone</td></tr><tr class="even"><td class=" sorting_1">!okno</td><td class="">Umyjmy je razem! http://i.imgur.com/ZhSQpkn.jpg</td><td class="">Everyone</td></tr><tr class="odd"><td class=" sorting_1">!papug</td><td class="">Jam jest Król Papug IV (づ｡◕‿‿◕｡)づ Witajcie poddani</td><td class="">Everyone</td></tr><tr class="even"><td class=" sorting_1">!photoshop</td><td class="">Burżuazja!</td><td class="">Everyone</td></tr><tr class="odd"><td class=" sorting_1">!plugdj</td><td class="">https://plug.dj/kagaminium</td><td class="">Everyone</td></tr><tr class="even"><td class=" sorting_1">!poważnie</td><td class="">Bądźmy poważni KappaHD http://i.imgur.com/wsfcKcw.gif</td><td class="">Everyone</td></tr><tr class="odd"><td class=" sorting_1">!rekord</td><td class="">https://www.youtube.com/watch?v=BpHSm0KcW7o Aktualny rekord: 371 widzów.</td><td class="">Moderator</td></tr><tr class="even"><td class=" sorting_1">!rngesus</td><td class="">(つ°ヮ°)つ Dziękuję Ci Bogu RNG! http://i.imgur.com/RbPiQsL.gif</td><td class="">Everyone</td></tr><tr class="odd"><td class=" sorting_1">!rngfail</td><td class="">RNG skraca mi zycie ~Kagami</td><td class="">Everyone</td></tr><tr class="even"><td class=" sorting_1">!salut</td><td class="">[touser] Welcome in the Papug's army Soldier! Salut!</td><td class="">Moderator</td></tr><tr class="odd"><td class=" sorting_1">!stfu</td><td class="">Oh shut your face http://youtu.be/18kqcczy6MQ?t=12s</td><td class="">Everyone</td></tr><tr class="even"><td class=" sorting_1">!sylvanas</td><td class="">FUUUUUUUUUUUUUUU http://i.imgur.com/2zrhzss.gif</td><td class="">Everyone</td></tr><tr class="odd"><td class=" sorting_1">!szafa</td><td class="">Kocham Cię szafo! Tak ładnie pachniesz! http://i.imgur.com/4diyHYX.gif</td><td class="">Everyone</td></tr><tr class="even"><td class=" sorting_1">!toplay</td><td class="">Przypominajka - do zagrania na streamie: CS:GO z widzami, Mount &amp; Blade, Valiant Hearts, To The Moon, 0A.D., K&amp;M, Civ 5 z widzami, TeeWorlds z widzami, Tzar, Heroes 3</td><td class="">Regular</td></tr><tr class="odd"><td class=" sorting_1">!ts</td><td class="">ts.23games.me</td><td class="">Everyone</td></tr><tr class="even"><td class=" sorting_1">!zezwól</td><td class="">[alias]</td><td class="">Moderator</td></tr><tr class="odd"><td class=" sorting_1">król</td><td class="">Jam jest Król(owa) Papug IV (づ｡◕‿‿◕｡)づ Witajcie poddani</td><td class="">Everyone</td></tr></tbody></table>
+            </div>
 		</div>
 
 		<!-- Twitch (Live Stream) - div górny -->
@@ -77,10 +82,10 @@
 
 		<!-- Brak treści - div dolny -->
 		<div id="Bottom" class="leaguegothic white page">
-			<div id="logo" class="f2">^hello^ {owner}</div></a>
-			<div class="f0 center w750 pad">
-
-			</div>
+			<div id="logo" class="f2" style="padding-bottom: 50px">^hello^ {owner}</div></a>
+			<div class="black whitebg calibri f0 center w750 pad">
+                <div class="fb-comments" data-href="http://kagaminium.23games.me" data-width="100%" data-numposts="10" data-colorscheme="dark"></div>
+            </div>
 		</div>
 
 		<!-- Fuknkcja Testowa -->
